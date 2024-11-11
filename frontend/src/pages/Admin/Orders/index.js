d8654846f4ac90f747c87d35099f321e8b9b71d7
Thanh Thuy <thuy104552@donga.edu.vn>
2024-11-11 16:14:00 +0700

    update

--- frontend/src/pages/Admin/Orders/index.js
+++ frontend/src/pages/Admin/Orders/index.js
@@ -53,7 +53,7 @@ function Orders() {
             <Tr>
               <Th>Users</Th>
               <Th>Address</Th>
-              <Th>Items</Th>
+              <Th textAlign="center">Items</Th>
             </Tr>
           </Thead>
           <Tbody>
