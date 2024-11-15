99a8717db61ce7f752a053c0bb202c27b18bb90b
VietTrong <trong106407@donga.edu.vn>
2024-11-15 17:13:33 +0700

    Update index.js

--- frontend/src/pages/Auth/Signup/index.js
+++ frontend/src/pages/Auth/Signup/index.js
@@ -42,7 +42,7 @@ function Signup({ history }) {
       <Flex align="center" width="full" justifyContent="center">
         <Box pt={10}>
           <Box textAlign="center">
-            <Heading>Sign upp</Heading>
+            <Heading>Sign uppp</Heading>
           </Box>
           <Box my={5}>
             {formik.errors.general && (
