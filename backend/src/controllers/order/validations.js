4411f9461588bf50f6d4da3461d0e35ec06b05a9
taquocy <taquocitali@gmail.com>
2025-01-03 13:38:51 +0700

    update code for  file validtion.js

--- backend/src/controllers/order/validations.js
+++ backend/src/controllers/order/validations.js
@@ -1,5 +1,12 @@
 import Joi from 'joi';
 
+/**
+ * Schema for validating order data.
+ * 
+ * @typedef {Object} OrderSchema
+ * @property {string} address - The address for the order. This field is required.
+ * @property {string[]} items - An array of item identifiers. This field is required.
+ */
 const OrderSchema = Joi.object({
   address: Joi.string().required(),
   items: Joi.array().items(Joi.string()).required(),
